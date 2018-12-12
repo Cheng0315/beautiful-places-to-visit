a3cb65747319d7fd802cf0669cb00e56b68beda5
Cheng Vue <cvu3@ucsc.edu>
2018-12-12 11:06:06 +0000

    first commit

---
+++ lib/beautiful_places_to_visit.rb
@@ -0,0 +1,6 @@
+require "beautiful_places_to_visit/version"
+
+module BeautifulPlacesToVisit
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/beautiful_places_to_visit/version.rb
@@ -0,0 +1,3 @@
+module BeautifulPlacesToVisit
+  VERSION = "0.1.0"
+end
