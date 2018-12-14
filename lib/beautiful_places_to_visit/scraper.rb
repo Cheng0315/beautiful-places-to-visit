c47274cc9e1c006e23060f57d87c6c4a1ca79bee
Cheng Vue <cvu3@ucsc.edu>
2018-12-14 15:13:33 +0000

    first done with two create destination methods

--- lib/beautiful_places_to_visit/scraper.rb
+++ lib/beautiful_places_to_visit/scraper.rb
@@ -4,20 +4,21 @@ class BeautifulPlacesToVisit::Scraper
     Nokogiri::HTML(open("http://time.com/money/page/best-places-to-travel-2018/"))
   end
 
-  def create_us_destionations
-    scrape_page.css("section.chapter h2").text
-    destinations = scrape_page.css("section.chapter h2:not(h1:nth-of-type(3) h2)").text:not(h2)").collect {|d| d.text}
-    costs_arr = scrape_page.css("section.chapter h2 + p").collect {|i| i.text.scan(/\$\d+,?\d+/)}
-    description = scrape_page.css("section.chapter h2 + p + p").collect {|i| i.text}
-    images = scrape_page.css("section.chapter img").collect {|i| i.attribute("src").value}.reject.with_index{|e, i| i == 10 || i ==11}
-    number_of_places = scrape_page.css("section.chapter h2").collect {|name| name.text}.count
+  def create_us_destinations
+    range = scrape_page.css("section.chapter h2").collect {|d| d.text}.count - scrape_page.css("section.chapter h1:nth-of-type(3) ~ h2").collect {|d| d.text}.count
+    destinations = scrape_page.css("section.chapter h2").collect {|d| d.text}.slice(0, range)
+    costs_arr = scrape_page.css("section.chapter h2 + p").collect {|i| i.text.scan(/\$\d+,?\d+/)}.slice(0, range)
+    description = scrape_page.css("section.chapter h2 + p + p").collect {|i| i.text}.slice(0, range)
+    images = scrape_page.css("section.chapter img").collect {|m| m.attribute("src").value}.reject.with_index{|e, i| i == 10 || i ==11}.slice(0, range)
   end
 
   def create_international_destionations
+    range = scrape_page.css("section.chapter h2").collect {|d| d.text}.count - scrape_page.css("section.chapter h1:nth-of-type(3) ~ h2").collect {|d| d.text}.count
+    max_range = scrape_page.css("section.chapter h2").collect {|d| d.text}.count
+
     destinations = scrape_page.css("section.chapter h1:nth-of-type(3) ~ h2").collect {|d| d.text}
     costs_arr = scrape_page.css("section.chapter h1:nth-of-type(3) ~ h2 + p").collect {|c| c.text.scan(/\$\d+,?\d+/)}
     description = scrape_page.css("section.chapter h1:nth-of-type(3) ~ h2 + p + p").collect {|t| t.text}
-    images = scrape_page.css("section.chapter img").collect {|m| m.attribute("src").value}.reject.with_index{|e, i| i == 10 || i ==11}
-
+    images = scrape_page.css("section.chapter img").collect {|m| m.attribute("src").value}.reject.with_index{|e, i| i == 10 || i == 11}.slice(range, max_range)
   end
 end
