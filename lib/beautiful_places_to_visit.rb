00836aceeaab51cd3f07e4e264fb3350cd568aee
Cheng Vue <cvu3@ucsc.edu>
2018-12-14 12:07:32 +0000

    with us and int medthods

--- lib/beautiful_places_to_visit.rb
+++ lib/beautiful_places_to_visit.rb
@@ -5,6 +5,7 @@ require "open-uri"
 require_relative "./beautiful_places_to_visit/version"
 require_relative "./beautiful_places_to_visit/cli"
 require_relative "./beautiful_places_to_visit/destination"
+require_relative "./beautiful_places_to_visit/scraper"
 
 module BeautifulPlacesToVisit
   class Error < StandardError; end
--- lib/beautiful_places_to_visit/scraper.rb
+++ lib/beautiful_places_to_visit/scraper.rb
@@ -1,18 +1,23 @@
 class BeautifulPlacesToVisit::Scraper
 
   def scrape_page
-    doc = Nokogiri::HTML(open("http://time.com/money/page/best-places-to-travel-2018/"))
-    destination = doc.css("section.chapter h2").text
-    binding.pry
+    Nokogiri::HTML(open("http://time.com/money/page/best-places-to-travel-2018/"))
   end
 
-  def get_us
-    scrape_page.collect do |destination|
-      destination_hash = {}
-      destination_hash[:destination] = "i"
-      destination_hash
-    end
+  def create_us_destionations
+    scrape_page.css("section.chapter h2").text
+    destinations = scrape_page.css("section.chapter h2:not(h1:nth-of-type(3) h2)").text:not(h2)").collect {|d| d.text}
+    costs_arr = scrape_page.css("section.chapter h2 + p").collect {|i| i.text.scan(/\$\d+,?\d+/)}
+    description = scrape_page.css("section.chapter h2 + p + p").collect {|i| i.text}
+    images = scrape_page.css("section.chapter img").collect {|i| i.attribute("src").value}.reject.with_index{|e, i| i == 10 || i ==11}
+    number_of_places = scrape_page.css("section.chapter h2").collect {|name| name.text}.count
   end
 
+  def create_international_destionations
+    destinations = scrape_page.css("section.chapter h1:nth-of-type(3) ~ h2").collect {|d| d.text}
+    costs_arr = scrape_page.css("section.chapter h1:nth-of-type(3) ~ h2 + p").collect {|c| c.text.scan(/\$\d+,?\d+/)}
+    description = scrape_page.css("section.chapter h1:nth-of-type(3) ~ h2 + p + p").collect {|t| t.text}
+    images = scrape_page.css("section.chapter img").collect {|m| m.attribute("src").value}.reject.with_index{|e, i| i == 10 || i ==11}
 
+  end
 end
